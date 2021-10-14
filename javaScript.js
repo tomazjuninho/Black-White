4e42662a26e6fdfb9696aa92e20451bb48c6b695
TOMAZ FERREIRA GOMES JUNIOR <tomaz.gomes@hotmail.com>
2021-10-14 09:27:28 -0300

    first commit

---
+++ javaScript.js
@@ -0,0 +1,46 @@
+
+
+const instagram = document.querySelector('.insta')
+
+
+function instaOn() {
+
+    instagram.src = "https://img.icons8.com/fluency/47/000000/instagram-new.png"
+}
+
+function instaOf() {
+
+    instagram.src = "https://img.icons8.com/carbon-copy/50/000000/instagram-new--v1.png"
+}
+
+
+
+
+const facebook = document.querySelector('.face')
+
+function faceOn() {
+
+    facebook.src = "https://img.icons8.com/color/45/000000/facebook.png"
+}
+
+function faceOf() {
+
+    facebook.src = "https://img.icons8.com/carbon-copy/50/000000/facebook.png"
+}
+
+
+
+const whatsapp = document.querySelector('.whats')
+
+function whatsOn() {
+
+    whatsapp.src = "https://img.icons8.com/office/40/000000/whatsapp--v1.png"
+}
+
+function whatsOf() {
+
+    whatsapp.src = "https://img.icons8.com/carbon-copy/50/000000/whatsapp--v1.png"
+}
+
+
+
